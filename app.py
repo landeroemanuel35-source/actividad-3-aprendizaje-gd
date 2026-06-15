49cc47ae3b9a65662e9eaf7d9a03c228d494f29a
landeroemanuel35-source <landeroemanuel35@gmail.com>
2026-06-15 13:32:48 -0600

    Update app.py

--- app.py
+++ app.py
@@ -86,7 +86,10 @@ def new_model():
 # =========================================================
 # PARÁMETROS
 # =========================================================
-bucket_name = st.text_input("Bucket de GCS:", "bucket_131025")
+bucket_name = st.text_input(
+    "Bucket de GCS:",
+    "bucket-grandes-datos-15042026-emanuel"
+)
 prefix = st.text_input("Prefijo/carpeta:", "tlc_yellow_trips_2022/")
 limite = st.number_input("Filas a procesar por archivo:", value=1000, step=100)
 
